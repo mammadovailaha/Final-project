e61a6a366fc5ef6fcea907b630d3ea9a96e5fc72
mammadovailaha <ilamemmedova14@gmail.com>
2025-07-01 12:37:08 +0400

    some ui update

--- src/components/BloqCard.tsx
+++ src/components/BloqCard.tsx
@@ -10,11 +10,11 @@ interface Props{
 
 const BloqCard:React.FC<Props> = ({src, alt, key, text}) => {
   return (
-    <div className='w-94 h-40 md:w-120 md:h-50 flex justify-center items-center'>
-      <div className='w-47 md:w-67 h-full bg-amber-50' key={key}><img className='w-full h-full object-contain' src={src} alt={alt} /></div>
+    <div className='w-94 h-40 md:w-120 md:h-50 flex justify-center items-center rounded-tl-[80px] md:rounded-tl-[100px] rounded-br-[80px] md:rounded-br-[100px] overflow-hidden bg-white shadow-lg hover:shadow-2xl transition-shadow duration-300 group relative'>
+      <div className='w-47 md:w-67 h-full bg-amber-50 ' key={key}><img className='w-full h-full object-contain' src={src} alt={alt} /></div>
       <div className='w-55 md:w-70 h-full bg-[#22caba] flex flex-col justify-center items-center gap-2 md:gap-3.5'>
         <p className='text-lg md:text-2xl text-white text-wrap text-center'>{text}</p>
-       <MoreInfoButton className='w-20 md:w-26 h-6 md:h-9 outline-none border border-white  text-white text-sm  md:text-lg rounded-[5px] hover:bg-white hover:text-[#22caba] transition-all duration-500'/>
+       <MoreInfoButton text='Daha ətraflı' className='w-20 md:w-26 h-6 md:h-9 outline-none border border-white  text-white text-sm  md:text-lg rounded-[5px] hover:bg-white hover:text-[#22caba] transition-all duration-500'/>
        </div>
     </div>
   )
--- src/components/StudyAbroadCard.tsx
+++ src/components/StudyAbroadCard.tsx
@@ -12,7 +12,7 @@ const StudyAbroadCard: React.FC<Props> = ({key,src, alt, country }) => {
     <div className="w-62 h-50 md:w-56 md:h-44  bg-[#DB133C] rounded-[7px] flex flex-col justify-end items-center"   style={{
     boxShadow: "-2px 6px 22px 3px rgba(0,0,0,0.25)"
   }}>
-      <div className="w-full h-48 md:h-42 bg-white hover:bg-[#22C8BA] transition-all duration-500 rounded-br-sm rounded-bl-sm flex flex-col justify-center items-center gap-2.5">
+      <div className="w-full h-48 md:h-42 bg-white group hover:bg-[#22C8BA] transition-all duration-500 rounded-br-sm rounded-bl-sm flex flex-col justify-center items-center gap-2.5">
         <div className="w-34 md:w-30 h-24 md:h-20" >
           <img
           className="w-full h-full"
@@ -21,7 +21,7 @@ const StudyAbroadCard: React.FC<Props> = ({key,src, alt, country }) => {
             alt={alt}
           />
         </div>
-        <p>{country}</p>
+        <p className="group-hover:text-white text-sm md:text-[16px]">{country}</p>
       </div>
     </div>
   );
