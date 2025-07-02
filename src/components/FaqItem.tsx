efbee5264332eb0aa33add68d34b48be03ad7e25
mammadovailaha <ilamemmedova14@gmail.com>
2025-07-02 18:24:49 +0400

    faq item question height

--- src/components/FaqItem.tsx
+++ src/components/FaqItem.tsx
@@ -19,10 +19,10 @@ const FaqItem: React.FC<FaqItemProps> = ({ id, question, answer, isOpen, onToggl
       <div
         onClick={() => onToggle(id)}
         className={`w-full flex justify-center items-center rounded-3xl ${
-          isOpen ? "h-5 md:h-18 bg-[#b0d8f0]" : "h-full bg-[#c8edf8]"
+          isOpen ? "h-10 md:h-18 bg-[#b0d8f0]" : "h-full bg-[#c8edf8]"
         } p-4 cursor-pointer transition-colors duration-700 ease-in-out`}
       >
-        <div className="w-[95%] flex justify-between items-center">
+        <div className="w-[95%] h-full flex justify-between items-center">
           <p className=" text-xs md:text-[16px] text-wrap ">{question}</p>
           <div className="cursor-pointer text-2xl text-[#015715] hover:text-[#22caba] transition-colors duration-700 ease-in-out">
             {isOpen ? <IoIosRemoveCircleOutline className="text-lg md:text-2xl" /> : <IoIosAddCircleOutline className="text-lg md:text-2xl" />}
