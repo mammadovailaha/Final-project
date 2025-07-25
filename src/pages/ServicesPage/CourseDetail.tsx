ed4f7ae12b8eb547eabab1565369fb79b60dd49c
mammadovailaha <ilamemmedova14@gmail.com>
2025-07-25 17:42:50 +0400

    err fix

--- src/pages/ServicesPage/CourseDetail.tsx
+++ src/pages/ServicesPage/CourseDetail.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 import ScrollAnimation from '../../components/ScrollAnimation'
 
 const CourseDetail = () => {
   return (
     <ScrollAnimation>
         <div>
-            
+
         </div>
     </ScrollAnimation>
   )
